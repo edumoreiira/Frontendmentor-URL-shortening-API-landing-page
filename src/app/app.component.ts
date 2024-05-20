07c507bc2f78d907d0337ec31da1d60c91356e8c
Eduardo Moreira <dudu_moreira9@hotmail.com>
2024-05-20 15:09:10 -0300

    create cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { MainBannerComponent } from './components/main-banner/main-banner.component';
 import { ShortenerComponent } from './components/shortener/shortener.component';
+import { CardsComponent } from './components/cards/cards.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, MainBannerComponent, ShortenerComponent],
+  imports: [RouterOutlet, NavbarComponent, MainBannerComponent, ShortenerComponent, CardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
